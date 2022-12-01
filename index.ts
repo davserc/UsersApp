64c79a61d8321086ff3f7367916836fe722174d6
David <daserc@gmail.com>
2022-11-30 22:17:55 -0300

    cambio de directorio index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-import { AppDataSource } from "./src/data-source"
-import { Users } from "./src/entity/Users"
 export { singUp,
          singIn,
          updateUser,
@@ -8,16 +6,3 @@ export { singUp,
          authenticateMail,
          forgotPasswordMail,
          resetPassword } from "./src/server/user.service"
-
-AppDataSource.initialize().then(async () => {
-    let userRepository = AppDataSource.getRepository(Users);
-    let finduser = await userRepository.findOne({where : { mail: "Admin@admin.com" }});
-    if(!finduser){
-       const user = new Users()
-       user.mail = "Admin@admin.com"
-       user.userName = "admin"
-       user.password = "admin"
-       await AppDataSource.manager.save(user)
-    }
-
-}).catch(error => console.log(error))
